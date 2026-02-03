eb26dfd886c92b3d9a8b182deaea0ee654bb87b2
Caleb <liucalebjiale@gmail.com>
2026-02-03 16:24:21 -0500

    test organization

---
+++ my-app/app/test/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import "./test.css";
+
+export default function Test() {
+  return (
+		<>
+			<div className="test">
+	bvnm,.
+			</div>
+			<div className="test2">dfsghj</div>
+			<div className="test2">dfsghj</div>
+			<div className="test2">dfsghj</div>
+			<div className="test2">dfsghj</div>
+		</>
+	);
+}
