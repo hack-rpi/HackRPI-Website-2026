3f21bfd48addddf4cfacc11a7d155164ed025837
Ben <bkudarauskas@gmail.com>
2026-02-03 17:40:11 -0500

    Frame for main page

---
+++ my-app/app/components/nav-bar/nav-bar.tsx
@@ -0,0 +1,8 @@
+
+export default function NavBar() {
+	return (
+		<div className="bg-gray-700 w-full h-20 opacity-50 fixed">
+			Desktop Nav Bar
+		</div>
+	);
+}
---
+++ my-app/app/components/title-components/desktop-title.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function DesktopTitleComponent() {
+	return (
+		<div className="w-full h-screen bg-gray-600 p-5">
+			Title
+		</div>
+	);
+}
