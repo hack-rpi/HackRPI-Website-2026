81bbf7385571924bcc94cfd8f50598596b156e04
suresg <suresg@rpi.edu>
2026-02-03 18:39:13 -0500

    prize directory
    Set up prize directory and necessary files

---
+++ my-app/app/prizes/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import "./prizes.css";
+
+export default function Test() {
+  return (
+		<>
+			<div className="test">
+	bvnm,.
+			</div>
+			<div className="test2">dfsghj</div>
+			<div className="test2">dfsghj</div>
+			<div className="test2">dfsghj</div>
+			<div className="test2">dfsghj</div>
+		</>
+	);
+}
