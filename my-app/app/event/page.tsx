da9adb62c997d1a34b82b612ea726c880bb98ecf
Steven Luo <luos4@rpi.edu>
2026-02-04 21:06:04 -0500

    Major changes to event page to match homepage

--- my-app/app/event/page.tsx
+++ my-app/app/event/page.tsx
@@ -1,58 +1,49 @@
 'use client'
 
-import "./event.css"
+import Footer from "@/app/components/footer/footer";
+import NavBar from "@/app/components/nav-bar/nav-bar";
 
 export default function Test() {
   return (
 		<>
-      <main className = "flex flex-col gap-3 h-screen">
-        <div className = "tempNavBar">
-          NAVBAR COMPONENT
-        </div>
-
-        <div className="tempHeader">
+      <NavBar />
+      <main className = "flex flex-col w-full">
+        <div className="w-full h-[75vh] bg-gray-500 p-5">
           Event Information
-          HEADER COMPONENT - should probably be like a function, where the title
-          is modifiable
-        </div>
-
-        CSS obviously not done
-        <div className="border-2 border-red-500 flex p-[1em] justify-around locationInfo">
-          <div className="border-2 border-orange-500 flex flex-col p-[1em]">
-            <div className="border-2 border-cyan-500 p-[1em]">
-              Event top left
+          <div className = "h-[90%] flex p-[1em] justify-around locationInfo">
+            <div className="w-[40%] border-2 border-orange-500 flex flex-col justify-around p-[1em]">
+              <div className="border-2 border-cyan-500 p-[1em]">
+                Event top left
+              </div>
+              <div className="border-2 border-cyan-500 p-[1em]">
+                Event bottom left
+              </div>
             </div>
-            <div className="border-2 border-cyan-500 p-[1em]">
-              Event bottom left
-            </div>
-          </div>
-          <div className="border-2 border-pink-500 flex flex-col p-[1em]">
-            <div className="border-2 border-cyan-500 p-[1em]">
-              Buttons
-            </div>
-            <div className="border-2 border-cyan-500 p-[1em]">
-              Map
+            <div className="w-[40%] border-2 border-orange-500 flex flex-col justify-around p-[1em]">
+              <div className="border-2 border-cyan-500 p-[1em]">
+                Buttons
+              </div>
+              <div className="border-2 border-cyan-500 p-[1em]">
+                Map
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="border-2 border-green-500 mentorInfo">
-          NEED HELP? section. idk how to format this one now
+        <div className="w-full h-[30vh] bg-gray-400 p-5">
+          "Need help" section
         </div>
 
-        <div className="border-2 border-blue-500 judgingInfo">
-          JUDGING INFO
-        </div>
-
-        <div className="border-2 border-cyan-500 submissionInfo">
-          SUBMISSION INFO
-        </div>
-        
-
-        <div className="tempFooter">
-          FOOTER COMPONENT
+        <div className="w-full h-[40vh] bg-green-200 text-black p-5 flex flex-col">
+          <div className="w-full border-2 border-orange-500 p-[1em]">
+            JUDGING INFO
+          </div>
+          <div className="w-full border-2 border-orange-500 p-[1em]">
+            SUBMISSION INFO
+          </div>
         </div>
       </main>
+      <Footer/>
 		</>
 	);
 }
