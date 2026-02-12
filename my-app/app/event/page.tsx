686572fe20f6e0d94e5e0d106d0ca2c683116fde
Steven Luo <stevenluo2007@gmail.com>
2026-02-12 08:31:57 -0500

    Add some location info from HackRPI 2025

--- my-app/app/event/page.tsx
+++ my-app/app/event/page.tsx
@@ -14,25 +14,80 @@ export default function Event() {
 		<>
       <NavBar />
       <main className = "flex flex-col w-full">
-        <div className="w-full h-[75vh] bg-gray-500 p-5">
-          Event Information
+        <div className="w-full h-auto bg-gray-500 p-5">
+          <div className="p-5 text-xl">
+            Event Information
+          </div>
+
+          {/* Div to separate left and right sections */}
           <div className = "h-[90%] flex p-[1em] justify-around locationInfo">
+
+            {/* Left */}
             <div className="w-[40%] border-2 border-orange-500 flex flex-col justify-around p-[1em]">
+              {/* Top  */}
               <div className="border-2 border-cyan-500 p-[1em]">
-                Event top left
+                Address:{" "}
+								<a
+									href="https://maps.google.com/?q=Darrin+Communications+Center+51+College+Ave+Troy+NY+12180"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-blue-500 hover:underline"
+								>
+									Darrin Communications Center, Troy, NY 12180
+								</a>
+
+                <div className="w-full h-96">
+                  <iframe
+                    width="100%"
+                    height="100%"
+                    style={{ border: 0 }}
+                    src="https://maps.google.com/?q=Darrin+Communications+Center+51+College+Ave+Troy+NY+12180&output=embed"
+                    allowFullScreen
+                  ></iframe>
+                </div>
               </div>
+
+              {/* Bottom */}
               <div className="border-2 border-cyan-500 p-[1em]">
-                Event bottom left
+                <div className="text-2xl mt-10">
+                  <h3 className="font-bold text-4xl font-neutral">Free Parking</h3>
+                  <p className="">
+                    Parking is available at North Hall Parking Lot, 2-minute walk to Darrin Communications Center
+                  </p>
+                  <p className="">
+                    Parking Address:{" "}
+                    <a
+                      href="https://maps.google.com/?q=North+Lot+Troy+NY+12180"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-500 hover:underline"
+                    >
+                      North Lot, Troy, NY 12180
+                    </a>
+                  </p>
+                </div>
+                <div className="mt-4 text-2xl">
+                  <p>
+                    Be sure to check-in with our organizers at the organizing team table in the front of the DCC.
+                    Participants will be given a wrist band at check-in which will grant access to food, and activities.
+                    Check-in is open throughout the event!
+                  </p>
+                </div>
               </div>
             </div>
+
+            {/* Right  */}
             <div className="w-[40%] border-2 border-orange-500 flex flex-col justify-around p-[1em]">
+              {/* Top  */}
               <div className="border-2 border-cyan-500 p-[1em]">
                 Buttons
               </div>
+              {/* Bottom */}
               <div className="border-2 border-cyan-500 p-[1em]">
                 Map
               </div>
             </div>
+
           </div>
         </div>
 
