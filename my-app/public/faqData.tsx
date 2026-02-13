39284aa7319bc3cba1dba0e543b324c6b43c1579
Steven Luo <stevenluo2007@gmail.com>
2026-02-13 16:44:05 -0500

    Update FAQ data to be more condensed
    Combined some of the questions and formatted the answers

--- my-app/public/faqData.tsx
+++ my-app/public/faqData.tsx
@@ -1,24 +1,27 @@
-// import RegistrationButton from "@/components/themed-components/registration-link";
+// import RegistrationButton from "@/components/themed-components/registration-link"; // FIXME
 
 type FAQ = {
 	title: string;
 	content: React.ReactNode;
 };
+
 const faqs: FAQ[] = [
 	{
-		title: "What is HackRPI?",
+		title: "What/when/where is HackRPI?",
 		content:
-			"Teams of 1-4 have 24 hours to build a project relating to our theme Retro V. Modern. Teams will then present their projects, and the best projects win prizes!",
-	},
-	{
-		title: "When is HackRPI?",
-		content:
-			"HackRPI takes place on November 15th and 16th, 2025. Arrival and check-in takes place from 9-10 AM. Our opening ceremony starts at 10 AM, and hacking begins at 11am. Your projects must be on Devpost by 10 AM Sunday, and all coding must stop at 11 AM Sunday. Teams will present, and the event will end around 4PM on Sunday. We are excited to see you there!",
-	},
-	{
-		title: "Where is HackRPI?",
-		content:
-			"HackRPI takes place at Rensselaer Polytechnic Institute, in the Darrin Communication Center. Darrin Communications Center, 51 College Ave, Troy, NY 12180. See our event information page for more details.",
+			<p>
+				HackRPI takes place on __month__ _day_ and __day+1__, 2026, in the Darrin Communication Center
+				at Rensselaer Polytechnic Institute.
+				<br />
+				Arrival and check-in takes place from 9-10 AM. Our opening ceremony starts at 10 AM, and hacking begins at 11am.
+				<br />
+				During hacking time, teams of 1-4 have 24 hours to build a project relating to our theme, In the Clouds.
+				Teams will then present their projects, and the best projects win prizes!
+				<br />
+				Your projects must be on Devpost by 10 AM Sunday, and all coding must stop at 11 AM Sunday.
+				<br />
+				The event will end around 4 PM on Sunday.
+			</p>
 	},
 	{
 		title: "Is HackRPI free to attend?",
@@ -29,24 +32,35 @@ const faqs: FAQ[] = [
 		content: (
 			<div className="flex items-start md:items-center justify-start flex-col md:flex-row">
 				<p className="pr-2">You can click here to register with Major League Hacking (MLH).</p>
-				{/* <RegistrationButton /> This is a custom button component will update later */}
+				{/* <RegistrationButton /> // FIXME This is a custom button component will update later*/}
 			</div>
 		),
 	},
 	{
-		title: "Who can participate?",
+		title: "Who can participate? What if I'm a first-timer? What if I'm under 18?",
 		content:
-			"Everyone is welcome to participate, whether you are a hackathon veteran or this is your first hackathon, this is a great opportunity to learn, experiment with new technologies, and grow your skills. Our team of experienced mentors is here to help you in every step of the way.",
-	},
-	{
-		title: "I'm under 18, can I still participate?",
-		content:
-			"Students under 18 are welcome to attend, but are not allowed to stay overnight in the sleep rooms. Students under the age of 17 must have an adult (21+) chaperone with them at all times during the event.",
+			<p>
+				Everyone is welcome to participate!
+				<br />
+				Whether you are a hackathon veteran or this is your first hackathon, this is a great opportunity
+				to learn, experiment with new technologies, and grow your skills.
+				<br />Our team of experienced mentors
+				is here to help you in every step of the way.
+				<br />
+				Students under 18 are welcome to attend, but are not allowed to stay overnight in the sleep rooms.
+				<br />
+				Students under the age of 17 must have an adult (21+) chaperone with them at all times during the event.
+			</p>
 	},
 	{
 		title: "Do I have to be an RPI student?",
 		content:
-			"No! HackRPI is open to everyone, students of all experience levels from all colleges and universities are welcome to attend. Did you know that students from over 45 other colleges attended HackRPI!",
+			<p>
+				No! HackRPI is open to everyone, students of all experience levels from all colleges and universities
+				are welcome to attend.
+				<br />
+				Did you know that students from over 45 other colleges attended HackRPI?
+			</p>
 	},
 	{
 		title: "Does HackRPI provide travel reimbursement?",
@@ -67,20 +81,22 @@ const faqs: FAQ[] = [
 		content: "Bring your team, your laptop, chargers, any hardware you need, and a good night's sleep!",
 	},
 	{
-		title: "What is the theme?",
-		//TODO: Change the description
-		content:
-			"The theme for 2025's HackRPI is Retro vs Modern. In a world where nostalgia meets innovation, our 12th annual hackathon, Retro vs Modern, invites creators to explore the contrasts and possibilities between the past and the future. Join us in shaping the future through a creative lens that honors the old while embracing the new.",
-	},
-	{
 		title: "Is it okay if I am late to the event?",
 		content:
-			"Yes! You can arrive at any time during the event, but we recommend arriving before 11 AM on Saturday. Remember, the later you are, the less time you have to work on your project!",
+			<p>
+				Yes! You can arrive at any time during the event, but we recommend arriving before 11 AM on Saturday.
+				<br />
+				Remember, the later you are, the less time you have to work on your project!
+			</p>
 	},
 	{
 		title: "When are submissions due?",
 		content:
-			"All projects must be submitted to Devpost by 10 AM on Sunday. You will be able to modify your submission until 11am. After 11an, no coding or changes to your project are allowed.",
+			<p>
+				All projects must be submitted to Devpost by 10 AM on Sunday.
+				<br />
+				You will be able to modify your submission until 11am. After 11an, no coding or changes to your project are allowed.
+			</p>
 	},
 	{
 		title: "How do I submit my project?",
@@ -91,7 +107,14 @@ const faqs: FAQ[] = [
 	{
 		title: "When and how will prizes be awarded?",
 		content:
-			"Prizes are announced at the closing ceremony, which will take place around 4 PM on Sunday. Physical prizes will be distributed during the closing ceremony. Winners of cash prizes will be contacted by our team after the event. ",
+			<p>
+				Prizes are announced at the closing ceremony, which will take place around 4 PM on Sunday.
+				<br />
+				Physical prizes will be distributed during the closing ceremony.
+				<br />
+				Winners of cash prizes will be contacted by our team after the event.
+			</p>
 	},
 ];
+
 export default faqs
