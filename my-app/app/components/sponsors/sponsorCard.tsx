1ffdd0b7c5929f232a5cd744b8d737ff8a195aaf
suresg <suresg@rpi.edu>
2026-03-13 17:23:22 -0400

    Update sponsorCard.tsx
    Decrease fidgeting effect.

--- my-app/app/components/sponsors/sponsorCard.tsx
+++ my-app/app/components/sponsors/sponsorCard.tsx
@@ -39,34 +39,45 @@ export default function SponsorCard({ name, tier, image }: any) {
   );
   
   function handleMove(e: React.MouseEvent<HTMLDivElement>) {
-    const rect = e.currentTarget.getBoundingClientRect();
+  const rect = e.currentTarget.getBoundingClientRect();
 
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
 
-    let percentX = (x / rect.width - 0.5) * 2;
-    let percentY = (y / rect.height - 0.5) * 2;
+  const margin = 8;
 
-    if (Math.abs(percentX) < 0.05) percentX = 0;
-    if (Math.abs(percentY) < 0.05) percentY = 0;
-
-    // 3D tilt
-    setRotateY(percentX * 25);
-    setRotateX(-percentY * 25);
-
-    // dynamic colored glow
-    const shadowX = -percentX * 30;
-    const shadowY = -percentY * 30;
-
-
-    const distance = Math.sqrt(percentX * percentX + percentY * percentY);
-    const blur = 30 + distance * 40;
-    const opacity = 0.25 + distance * 0.4;
-
-    setShadow(
-      `${shadowX}px ${shadowY + 20}px ${blur}px rgba(${shadowColor},${opacity})`
-    );
+  // Prevent jitter near edges
+  if (
+    x < margin ||
+    x > rect.width - margin ||
+    y < margin ||
+    y > rect.height - margin
+  ) {
+    return;
   }
+
+  let percentX = (x / rect.width - 0.5) * 2;
+  let percentY = (y / rect.height - 0.5) * 2;
+
+  if (Math.abs(percentX) < 0.05) percentX = 0;
+  if (Math.abs(percentY) < 0.05) percentY = 0;
+
+  // 3D tilt
+  setRotateY(percentX * 25);
+  setRotateX(-percentY * 25);
+
+  // dynamic colored glow
+  const shadowX = -percentX * 30;
+  const shadowY = -percentY * 30;
+
+  const distance = Math.sqrt(percentX * percentX + percentY * percentY);
+  const blur = 30 + distance * 40;
+  const opacity = 0.25 + distance * 0.4;
+
+  setShadow(
+    `${shadowX}px ${shadowY + 20}px ${blur}px rgba(${shadowColor},${opacity})`
+  );
+}
 
   function handleLeave() {
     setRotateX(0);
@@ -79,7 +90,7 @@ export default function SponsorCard({ name, tier, image }: any) {
     bg_color +
     " " +
     text_color +
-    " transition-transform duration-100 ease-in-out text-center flex items-center justify-center transition-all duration-1000 ease-out";
+    " transition-transform text-center flex items-center justify-center transition-all duration-450 ease-out";
 
   let style2 = "mt-3 text-sm font-semibold tracking-wide capitalize opacity-0 -translate-y-3 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0 " + text_color; 
   return (
