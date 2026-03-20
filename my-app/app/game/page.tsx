23bf8407e2396c4fad2c0a0991e4fd6c4d0533a9
Caleb <liucalebjiale@gmail.com>
2026-03-20 16:14:14 -0400

    envelope

--- my-app/app/game/page.tsx
+++ my-app/app/game/page.tsx
@@ -5,7 +5,7 @@ import NavBar from "@/app/components/nav-bar/nav-bar";
 
 export default function Game() {
   return (<>
-
+    {/* <NavBar /> */}
     <div style={{ width: "100vw", height: "100vh", display: "flex", flexDirection: "column", backgroundColor: "black" }}>
       <h1>Game to be inserted here later</h1>
     </div>
