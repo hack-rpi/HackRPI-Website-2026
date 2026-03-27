2b70f2fdc569c85933fbbd06a4f68a09814cc208
Steven Luo <stevenluo2007@gmail.com>
2026-03-26 21:09:00 -0400

    Fix opacity not affecting dropdowns

--- my-app/app/components/nav-bar/desktop/nav-bar-desktop-variant.tsx
+++ my-app/app/components/nav-bar/desktop/nav-bar-desktop-variant.tsx
@@ -3,14 +3,65 @@ import { NavGroup } from "../nav-bar-links";
 // import logo from "@/public/Retro_HackRPI_Logo.png";
 // import RegistrationButton from "@/components/themed-components/registration-header-link";
 
-import NavGroupComponent from "./nav-group";
 import Link from "next/link";
+import { Link as lin } from "../nav-bar-links";
+
+function NavLink({href, children, onClick}: {
+	href: string;
+	children: React.ReactNode;
+	onClick?: () => void;
+}) {
+	return (
+		<Link
+			className="
+				w-full whitespace-nowrap p-0.5 h-8 text-center text-lg bg-size-[0%_2px] bg-no-repeat
+				bg-bottom-left transition-all duration-200 bg-linear-to-r from-[#00ff7f] to-[#87CEEB]
+				hover:bg-size-[100%_2px] focus:bg-size-[100%_4px]
+			"
+			href={href}
+			onClick={onClick}
+		>
+			{children}
+		</Link>
+	);
+}
+
+function NavGroupComponent({ name, links }: { name: string; links: lin[] }) {
+	return (
+		<div className="dropdown dropdown-hover">
+			<div
+				role="link"
+				className="
+					text-lg xl:text-xl bg-size-[0%_2px] bg-no-repeat bg-bottom-left
+					transition-all duration-200 bg-linear-to-r from-[#00ff7f] to-[#87CEEB]
+					hover:bg-size-[100%_2px] focus:bg-size-[100%_4px] my-4 mx-2 whitespace-nowrap
+				"
+			>
+				<Link href={links[0].href}>{name}</Link>
+			</div>
+
+			<ul
+				tabIndex={-1}
+				className="
+					dropdown-content menu p-2 w-52
+					bg-linear-to-r from-white/60 from-50% to-sky-200/60
+					border-2 border-black z-50
+				"
+			>
+				{links.map((link) => (
+					<li key={link.href} className="my-1" role="link">
+						<NavLink href={link.href}>{link.children}</NavLink>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
 
 export default function DesktopNavBarVariant({ links }: { links: NavGroup[] }) {
-
   return (
     /*<div className="bg-gradient-to-r from-hackrpi-light-purple via-hackrpi-pink to-hackrpi-light-purple w-full h-16">*/
-    <div className="w-full h-16 bg-linear-to-r from-sky-200/80 via-white/80 to-sky-200/80 text-slate-800 backdrop-blur-sm">
+    <div className="w-full h-16 bg-linear-to-r from-sky-200/60 via-white/60 to-sky-200/60 text-slate-800 backdrop-blur-sm">
       <div
         className="flex justify-center lg:justify-center items-center h-full border-b-2 border-black z-50"
         role="navigation"
