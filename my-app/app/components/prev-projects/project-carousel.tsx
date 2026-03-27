b7af6322a42d94f2bb9efb84c1366203e93926d0
Steven Luo <stevenluo2007@gmail.com>
2026-03-27 17:10:27 -0400

    Edits to last year page carousel's style + Beginnings of photo page

--- my-app/app/components/prev-projects/project-carousel.tsx
+++ my-app/app/components/prev-projects/project-carousel.tsx
@@ -31,20 +31,22 @@ export default function ProjectCarousel({ projects }: { projects: ProjectCarouse
 				containerClass="w-full h-fit rounded-md mb-2"
 			>
 				{projects.map((project, index) => (
-					<div className="items-center flex-col w-full h-fit flex justify-center my-4 border-2 border-red-500" key={index}>
+					<div className="items-center flex-col w-full h-fit flex justify-center my-4" key={index}>
 						<h1 className="text-ellipsis w-full text-center text-xl xs:text-2xl sm:text-3xl font-bold font-sans p-1">
 							{project.prizeCategory}
 						</h1>
-						<div className="border-2 border-green-500 bg-transparent lightText w-full flex flex-col items-center justify-start bg-silver rounded-md m-4">
-							<h2 className="border-2 border-blue-500 w-full mb-2 text-center text-2xl font-bold font-sans">{project.title}</h2>
+						<div className="bg-transparent lightText w-full flex flex-col items-center justify-start bg-silver rounded-md m-4">
+							<h2 className="w-full mb-2 text-center text-2xl font-bold font-sans">{project.title}</h2>
 							<p className="mb-4 w-full text-center m">{project.authors.join(", \n")}</p>
-							<Image
-								src={project.imageUrl}
-								alt={project.title}
-								width={500}
-								height={500}
-								className="sizeImage z-0 object-cover w-full max-h-[45vh] mb-2 rounded-md"
-							></Image>
+							<div className="relative w-[100vh] h-[45vh]">
+								<Image
+									src={project.imageUrl}
+									alt={project.title}
+									fill
+									objectFit="cover"
+									className="sizeImage z-0 w-full mb-2 rounded-md"
+								></Image>
+							</div>
 							<div className="absolute sm:w-3/4 -top-50 flex-col w-full pl-4 mt-2 text-center"></div>
 							<p className="w-full px-4 font-sans text-center my-2">{project.description}</p>
 						</div>
