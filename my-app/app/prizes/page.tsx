67b24ef2e1d3131ba369c9d1f3fa64c40e5c3407
Ben <bkudarauskas@gmail.com>
2026-04-17 17:31:43 -0400

    Update page.tsx

--- my-app/app/prizes/page.tsx
+++ my-app/app/prizes/page.tsx
@@ -98,6 +98,13 @@ function SceneTransition() {
 }
 
 export default function prizes() {
+	const prizes = [
+		{ title: "title", description: "desc", prize: "$100", img: "" },
+		{ title: "title2", description: "desc2", prize: "$200", img: "" },
+		{ title: "title3", description: "desc3", prize: "$300", img: "" },
+		{ title: "title4", description: "desc4", prize: "$400", img: "" },
+	];
+
 	function textAnimation(id: string, speed: number = 1.0, delay: number = 0.0, effect: number = 0) {
 		// Pass an id for it to iterate though. It must have a child element like this:
 		// <div className="text-animation-layer inline-block w-auto" id="text-animate-layer"/>
@@ -161,6 +168,7 @@ export default function prizes() {
 		// Get elements
 		const sectionPin = document.querySelector("#pin");
 		const majorPrizes = document.querySelector("#majorPrizes");
+		const otherPrizes = document.querySelector("#otherPrizes");
 		// bg colors to change
 		const landing = document.querySelector("#landing");
 		const spacer = document.querySelector("#spacer");
@@ -181,7 +189,7 @@ export default function prizes() {
 		ScrollTrigger.create({
 			trigger: sectionPin,
 			start: "top top",
-			end: () => "+=" + (window.innerHeight * 3.5),
+			end: () => "+=" + (window.innerHeight * 7.5),
 			pin: true,
 			anticipatePin: 0, // make sure this is off or else it will anticipate it and jump the gun and make it not look smooth
 		});
@@ -197,11 +205,28 @@ export default function prizes() {
 			},
 		});
 
-		tlColor.fromTo(landing, { backgroundColor: "rgb(0,0,0)" }, { backgroundColor: "rgb(200,200,200)" , duration: 1, ease: "power1.inOut" }, 0.2);
-		tlColor.fromTo(spacer, { backgroundColor: "rgb(0,0,0)" }, { backgroundColor: "rgb(200,200,200)" , duration: 1, ease: "power1.inOut" }, 0.2);
-		tlColor.fromTo(majorPrizes, { backgroundColor: "rgb(0,0,0)" }, { backgroundColor: "rgb(200,200,200)" , duration: 1, ease: "power1.inOut" }, 0.2);
+		tlColor.fromTo(landing, { backgroundColor: "rgb(0,0,0)" }, { backgroundColor: "rgb(39, 86, 120)", duration: 1, ease: "power1.inOut" }, 0.2);
+		tlColor.fromTo(spacer, { backgroundColor: "rgb(0,0,0)" }, { backgroundColor: "rgb(39, 86, 120)", duration: 1, ease: "power1.inOut" }, 0.2);
+		tlColor.fromTo(majorPrizes, { backgroundColor: "rgb(0,0,0)" }, { backgroundColor: "rgb(39, 86, 120)", duration: 1, ease: "power1.inOut" }, 0.2);
 
 
+		const tlScroll = gsap.timeline({
+			scrollTrigger: {
+				trigger: sectionPin,
+				start: "top top",
+				end: () => "+=" + window.innerHeight * 7.5,
+				scrub: true,
+				anticipatePin: 0,
+			},
+		});
+
+		const prizeRows = gsap.utils.toArray<HTMLElement>(".animate-prizes");
+		tlScroll.fromTo(
+			prizeRows,
+			{ height: "5vh" },
+			{ height: "15vh", duration: 0.02, ease: "power1.inOut", stagger: 1.0 },
+			1.6,
+		);
 
 
 		return () => {
@@ -241,37 +266,37 @@ export default function prizes() {
 				<div className="h-[50vh] w-full flex items-center justify-center" id="spacer" />
 
 				<div className="w-full h-screen" id="pin">
-					<div className="h-[100vh] z-2 w-full absolute">
+					{/* <div className="h-[100vh] z-2 w-full absolute">
 						<Canvas dpr={[1, 1.5]} orthographic camera={{ position: [0, 0, 10], zoom: 100 }}>
 							<SceneTransition />
 						</Canvas>
-					</div>
+					</div> */}
 
 					<div className="w-full h-screen absolute flex text-center text-2xl font-sans" id="majorPrizes">
-						<div id="minor">
-							For minor prize tracks
+
+						<div className="absolute -bottom-24 w-full h-80 bg-blue-300 rounded-full mix-blend-multiply filter blur-3xl opacity-30"></div>
+						<div className="">
+
 						</div>
-						<div id="mlh">
-							For MLH prize tracks
-						</div>
-												
+
 					</div>
 
 					<div className="w-full h-screen absolute z-[-1] flex text-center text-2xl font-sans bg-[rgb(200,200,200)]" id="otherPrizes">
-						<div className="w-[200vh] bottom-[-50vh] right-240 aspect-square absolute rounded-full bg-gBlack"></div>
 						<div className="h-full w-2/5 justify-center items-center flex">
 							<div className="w-2/3 bg-gray-200 aspect-square z-[1]">
 								A
 							</div>
 						</div>
-						<div className="h-full w-3/5">
-							<div className="w-full h-full flex flex-col justify-center items-center">
-								<div className="w-5/6 h-1/6 bg-gray-500">
-									a
-								</div>
-								<div className="w-5/6 h-1/6 bg-gray-600">
-									b
-								</div>
+						<div className="h-full w-3/5 flex flex-col justify-center items-center">
+							<div className="w-5/6 flex flex-col justify-center items-center rounded-xl overflow-hidden">
+								{prizes.map((item, i) => (
+									<div key={i} className="animate-prizes w-full min-h-[5vh] bg-gray-500 flex items-center">
+										<span className="w-4/5 text-center">{item.title}</span>
+										<span className="w-1/5 text-center">{item.prize}</span>
+										<br/>
+										<span className="w-1/5 text-center">{item.description}</span>
+									</div>
+								))}
 							</div>
 						</div>
 					</div>
