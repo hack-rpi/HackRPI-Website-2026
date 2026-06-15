e31bc9c1a4310d3c7d7dd23c7d18f353e38c7cea
BastedEggsRYummy <baimej@rpi.edu>
2026-06-15 19:40:11 -0400

    Revert "Attempting the chat suggestions"
    This reverts commit 00ed3f8d941d73077726e79bfecf87a8727021ba.

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
   /* config options here */
 };
 
