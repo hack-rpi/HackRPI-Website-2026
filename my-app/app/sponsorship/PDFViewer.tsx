00e7a8de7a8ef87ea169f40ba60d27752ee7f1c3
Caleb <liucalebjiale@gmail.com>
2026-08-03 21:04:31 -0400

    new pdf viewer + changes to sponsorship page

---
+++ my-app/app/sponsorship/PDFViewer.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+// Make sure worker URL is configured
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+interface PDFViewerProps {
+  file: string;
+}
+
+export default function PDFViewer({ file }: PDFViewerProps) {
+  const [numPages, setNumPages] = useState<number>();
+
+  return (
+    <div className="flex flex-col items-center py-10">
+      <Document
+        file={file}
+        onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+      >
+        {Array.from(new Array(numPages), (_, index) => (
+          <Page 
+            key={`page_${index + 1}`} 
+            pageNumber={index + 1} 
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+          />
+        ))}
+      </Document>
+    </div>
+  );
+}
